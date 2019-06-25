e606addeabfb01177dacafc9901b699062a9e6bb
trottao <federico.trotta1992@gmail.com>
2019-06-25 15:39:28 +0200

    CateogoriaDAOImpl ComposizioneOrdiniImpl

---
+++ ProgettoFinale/src/dao/ComposizioneOrdiniDAOImpl.java
@@ -0,0 +1,80 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import bean.Categoria;
+import bean.ComposizioneOrdini;
+
+@Repository
+public class ComposizioneOrdiniDAOImpl implements ComposizioneOrdiniDAO{
+
+	@Autowired
+	private JdbcTemplate template;
+	
+	@Override
+	public void insert(ComposizioneOrdini com) {
+		template.update("insert into composizioneOrdine values(?,?,?)",com.getIdOrdine(),
+																	   com.getIdProdotto(),
+																	   com.getQuantita());
+		
+	}
+
+	@Override
+	public boolean delete(String idOrdine, int idProdotto) {
+		int n = template.update("delete from composizioneOrdine where idOrdine=? and idProdotto=?", idOrdine,idProdotto);
+		return n==1;
+		
+	}
+
+	@Override
+	public ComposizioneOrdini select(String idOrdine, int idProdotto) {
+		ComposizioneOrdini com = template.queryForObject("select * from composizioneOrdine where idOrdine=? and idProdotto=?", new ComposizioneOrdiniMapper(), idOrdine, idProdotto);
+		return com;
+	}
+
+	@Override
+	public List<ComposizioneOrdini> selectAllOrdini() {
+		List<ComposizioneOrdini> listaOrdini= template.query("select * from composizioneOrdine", new ComposizioneOrdiniMapper());
+		return listaOrdini;
+	}
+
+	@Override
+	public List<ComposizioneOrdini> selectAllOrdini(String where) {
+		List<ComposizioneOrdini> listaOrdini= template.query("select * from composizioneOrdine "+where, new ComposizioneOrdiniMapper());
+		return listaOrdini;
+	}
+
+	@Override
+	public void update(ComposizioneOrdini com) {
+		template.update("update ComposizioneOrdini set quantita=? where idOrdine=? and idProdotto=?",com.getQuantita(),
+																			  				         com.getIdOrdine(),
+																			  				         com.getIdProdotto());
+		
+	}
+
+}
+
+
+
+
+class ComposizioneOrdiniMapper implements RowMapper<ComposizioneOrdini> {
+
+	@Override
+	public ComposizioneOrdini mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		ComposizioneOrdini com = new ComposizioneOrdini();
+		
+		com.setIdOrdine(rs.getString("idOrdine"));
+		com.setIdProdotto(rs.getInt("idProdotto"));
+		com.setQuantita(rs.getInt("quantita"));
+				
+		return com;
+	}
+}
