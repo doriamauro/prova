fb60e6a0fcc0790c94478fe6141da671c923f5d4
academy <academy@academy-PC>
2019-06-25 11:26:19 +0200

    kk

--- ProgettoFinale/src/bean/Cliente.java
+++ ProgettoFinale/src/bean/Cliente.java
@@ -22,7 +22,7 @@ public class Cliente {
 	private Affidabile affidabile;
 
 
-	//prova Maurof
+	//prova Maurofh
 //>>>>>>> branch 'master' of https://github.com/doriamauro/prova.git
 	public Cliente() {
 	}
