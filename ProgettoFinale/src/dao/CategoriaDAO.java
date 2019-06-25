22904fd383d68e269262b3e1aa01539296f5a15b
academy <academy@academy-PC>
2019-06-25 15:36:12 +0200

    tom

--- ProgettoFinale/src/dao/CategoriaDAO.java
+++ ProgettoFinale/src/dao/CategoriaDAO.java
@@ -9,7 +9,7 @@ public interface CategoriaDAO {
 
 	public void insert(Categoria c);
 	public boolean delete(int idCategoria);
-	public Categoria select(String idCategoria);
+	public Categoria select(int idCategoria);
 	public List<Categoria> selectAllCategorie();
 	public List<Categoria> selectAllCategorie(String where);
 	public void update(Categoria c);
