2d111cafd9b895e390fb468253052053d02bcfa9
academy <academy@academy-PC>
2019-06-26 17:47:21 +0200

    aa

--- ProgettoFinale/src/dao/ComposizioneOrdiniDAOImpl.java
+++ ProgettoFinale/src/dao/ComposizioneOrdiniDAOImpl.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import bean.Categoria;
 import bean.ComposizioneOrdini;
-//
+////
 @Repository
 @Transactional
 public class ComposizioneOrdiniDAOImpl implements ComposizioneOrdiniDAO{
