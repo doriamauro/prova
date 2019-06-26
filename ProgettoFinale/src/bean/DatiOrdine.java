af88d2597466b1926539de5f78fd18005e57f71d
academy <academy@academy-PC>
2019-06-26 11:51:49 +0200

    tom

--- ProgettoFinale/src/bean/DatiOrdine.java
+++ ProgettoFinale/src/bean/DatiOrdine.java
@@ -1,10 +1,13 @@
 package bean;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import service.ProdottoServiceImpl;
 
 public class DatiOrdine {
 	
-	private ArrayList<Prodotto> prodotti;
+	private ArrayList<Prodotto> prodotti = new ArrayList<Prodotto>();
 	private String username;
 	private ModPagamento modPag;
 	private IndirizzoOrdine indOrd;
@@ -51,10 +54,35 @@ public class DatiOrdine {
 		this.indOrd = indOrd;
 	}
 
+	public double calcolaTotale() {
+		double costoSing = 0;
+		double costoTot = 0;
+		
+		for (Prodotto p: prodotti) {
+			
+			costoSing = p.getPrezzoUni() - (p.getPrezzoUni()*(p.getSconto()/100));
+			costoTot += costoSing*p.getDisponibilita();
+		}
+		return costoTot;
+	}
+	
+	public void variaQuantita(Prodotto p) {
+		int index = this.prodotti.indexOf(new Prodotto(p.getIdProdotto()));
+		if (index != -1) {
+			this.prodotti.get(index).setDisponibilita(this.prodotti.get(index).getDisponibilita()+p.getDisponibilita());
+			
+			if (this.prodotti.get(index).getDisponibilita()<=0) {
+				this.prodotti.remove(index);
+			}
+		}
+	}
+	
 	@Override
 	public String toString() {
 		return "DatiOrdine [prodotti=" + prodotti + ", username=" + username + ", modPag=" + modPag + ", indOrd="
 				+ indOrd + "]";
 	}
+	
+	
 		
 }
