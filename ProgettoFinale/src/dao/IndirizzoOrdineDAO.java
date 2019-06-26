84950c5456d444b44fabc095e8861d0d30cad8ad
trottao <federico.trotta1992@gmail.com>
2019-06-26 13:58:45 +0200

    ind

--- ProgettoFinale/src/dao/IndirizzoOrdineDAO.java
+++ ProgettoFinale/src/dao/IndirizzoOrdineDAO.java
@@ -9,7 +9,7 @@ public interface IndirizzoOrdineDAO {
 	public void insert(IndirizzoOrdine indord);
 	public boolean delete(String idIndOrdine);
 	public IndirizzoOrdine select(String idIndOrdine);
-	public int getProxID();
+	public int getProxID();  
 	public List<IndirizzoOrdine> selectAllIndirizzi();
 	public List<IndirizzoOrdine> selectAllIndirizzi(String where); 
 	public void update(IndirizzoOrdine indord);
